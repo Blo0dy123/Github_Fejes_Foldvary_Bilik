e529b0d63b34f5b72078855c5eb492ed9409dfd2
FöldváryFilip <FldvryFilip@402A-PC07>
2024-03-18 08:49:43 +0100

    Add project files.

---
+++ Github_Fejes_Foldvary_Bilik/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Github_Fejes_Foldvary_Bilik.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
